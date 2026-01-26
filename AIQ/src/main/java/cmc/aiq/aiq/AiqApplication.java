38cce5e6bc73237008cd51197ba4b3f6d2ea6b9f
yoojiseong <yujiseong588@gmail.com>
2026-01-26 17:41:05 +0900

    first commit

---
+++ AIQ/src/main/java/cmc/aiq/aiq/AiqApplication.java
@@ -0,0 +1,13 @@
+package cmc.aiq.aiq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiqApplication.class, args);
+    }
+
+}
