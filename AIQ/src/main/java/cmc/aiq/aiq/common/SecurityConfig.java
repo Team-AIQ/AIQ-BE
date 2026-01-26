3134db1c0bc66ddf69e86cfadf95fb43ee638653
yoojiseong <yujiseong588@gmail.com>
2026-01-26 20:40:20 +0900

    기본 CORS 정책 및 CSRF 비활성화

---
+++ AIQ/src/main/java/cmc/aiq/aiq/common/SecurityConfig.java
@@ -0,0 +1,21 @@
+package cmc.aiq.aiq.common;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http
+                .csrf(csrf -> csrf.disable()) // API 서버이므로 CSRF 비활성화
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll() // 일단은 모든 요청 허용 (개발용)
+                );
+        return http.build();
+    }
+}
