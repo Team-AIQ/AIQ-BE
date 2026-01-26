b4d3f40c098e77f6429dc12e37e664fd27621a3a
yoojiseong <yujiseong588@gmail.com>
2026-01-26 23:59:21 +0900

    Users테이블 created_at 컬럼에 생성 시간이 채워지지 않은 에러 발견 => AiqApplication.java에 @EnableJpaAuditing어노테이션 붙여서 Users.createdAt의 @CreatedDate 어노테이션 활성화 시켰습니다.

--- AIQ/src/main/java/cmc/aiq/aiq/AiqApplication.java
+++ AIQ/src/main/java/cmc/aiq/aiq/AiqApplication.java
@@ -2,8 +2,10 @@ package cmc.aiq.aiq;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class AiqApplication {
 
     public static void main(String[] args) {
