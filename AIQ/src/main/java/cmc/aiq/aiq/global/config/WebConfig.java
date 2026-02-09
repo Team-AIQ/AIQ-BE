20fcb5abb321969c5d2fe5e7d99eb5a0a5d2208d
yoojiseong <yujiseong588@gmail.com>
2026-02-09 16:21:01 +0900

    AI 채팅 구현 완료

--- AIQ/src/main/java/cmc/aiq/aiq/global/config/WebConfig.java
+++ AIQ/src/main/java/cmc/aiq/aiq/global/config/WebConfig.java
@@ -9,7 +9,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000",
+                        "http://192.168.219.101:3000",
+                        "https://*.ngrok-free.app")
                 .allowedMethods("GET", "POST","DELETE","OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
