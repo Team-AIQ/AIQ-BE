b0027d26732f95d5fcaa46a750dca10949c03806
yoojiseong <yujiseong588@gmail.com>
2026-07-12 20:50:42 +0900

    cors 정책을 합치기 위해 WebConfig 파일 주석처리

--- AIQ/src/main/java/cmc/aiq/aiq/global/config/WebConfig.java
+++ AIQ/src/main/java/cmc/aiq/aiq/global/config/WebConfig.java
@@ -6,14 +6,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry){
-        registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000",
-                        "http://192.168.219.101:3000",
-                        "https://*.ngrok-free.app")
-                .allowedMethods("GET", "POST","DELETE","OPTIONS","PATCH","PATCH")
-                .allowedHeaders("*")
-                .allowCredentials(true);
-    }
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry){
+//        registry.addMapping("/api/**")
+//                .allowedOrigins("http://localhost:3000",
+//                        "http://192.168.219.101:3000",
+//                        "https://*.ngrok-free.app")
+//                .allowedMethods("GET", "POST","DELETE","OPTIONS","PATCH","PATCH")
+//                .allowedHeaders("*")
+//                .allowCredentials(true);
+//    }
 }
