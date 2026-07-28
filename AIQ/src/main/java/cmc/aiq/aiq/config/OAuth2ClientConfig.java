61b08ad5c3ad11f273b1bdb06ae621e2aa78a967
yoojiseong <yujiseong588@gmail.com>
2026-07-28 11:47:08 +0900

    APPLE 로그인 기능 개발 _ OAuth2ClientConfig.java 삭제

--- AIQ/src/main/java/cmc/aiq/aiq/config/OAuth2ClientConfig.java
+++ AIQ/src/main/java/cmc/aiq/aiq/config/OAuth2ClientConfig.java
@@ -1,32 +1,32 @@
-package cmc.aiq.aiq.config;
-
-import cmc.aiq.aiq.global.security.oauth.AppleJwtUtils;
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
-
-@Configuration
-@RequiredArgsConstructor
-public class OAuth2ClientConfig {
-
-    private final AppleJwtUtils appleJwtUtils;
-
-    @Bean
-    public ClientRegistrationRepository clientRegistrationRepository(ClientRegistrationRepository clientRegistrationRepository) {
-        ClientRegistration appleRegistration = clientRegistrationRepository.findByRegistrationId("apple");
-
-        ClientRegistration customAppleRegistration = ClientRegistration.withClientRegistration(appleRegistration)
-                .clientSecret(appleJwtUtils.createClientSecret())
-                .build();
-
-        return new InMemoryClientRegistrationRepository(
-                customAppleRegistration,
-                clientRegistrationRepository.findByRegistrationId("google"),
-                clientRegistrationRepository.findByRegistrationId("naver"),
-                clientRegistrationRepository.findByRegistrationId("kakao")
-        );
-    }
-}
+//package cmc.aiq.aiq.config;
+//
+//import cmc.aiq.aiq.global.security.oauth.AppleJwtUtils;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.oauth2.client.registration.ClientRegistration;
+//import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+//import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+//
+//@Configuration
+//@RequiredArgsConstructor
+//public class OAuth2ClientConfig {
+//
+//    private final AppleJwtUtils appleJwtUtils;
+//
+//    @Bean
+//    public ClientRegistrationRepository clientRegistrationRepository(ClientRegistrationRepository clientRegistrationRepository) {
+//        ClientRegistration appleRegistration = clientRegistrationRepository.findByRegistrationId("apple");
+//
+//        ClientRegistration customAppleRegistration = ClientRegistration.withClientRegistration(appleRegistration)
+//                .clientSecret(appleJwtUtils.createClientSecret())
+//                .build();
+//
+//        return new InMemoryClientRegistrationRepository(
+//                customAppleRegistration,
+//                clientRegistrationRepository.findByRegistrationId("google"),
+//                clientRegistrationRepository.findByRegistrationId("naver"),
+//                clientRegistrationRepository.findByRegistrationId("kakao")
+//        );
+//    }
+//}
